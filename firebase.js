76975c1fc11d8b8532174f24f4db188aa97ef31e
Krzysztof Fila <umowy.fila@gmail.com>
2024-09-20 10:34:09 +0200

    Feature: Add firebase, env

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+/* eslint-enable no-unused-vars */
+
+// const app = initializeApp(firebaseConfig);
+
+// firebase.initializeApp(firebaseConfig);
+
+// const db = firebase.firestore();
+// export { db };
